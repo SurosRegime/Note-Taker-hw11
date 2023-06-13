4425445d68b6326d590495ea071ec8d7af012e3f
SurosRegime <ephrain926@hotmail.com>
2023-06-13 19:28:47 -0400

    fixed require('express')
    had previously forgotten to put ' ' around express

--- routes/html.js
+++ routes/html.js
@@ -1,13 +1,17 @@
-const express = require(express);
-const router = express.Router();
 const path = require('path');
+const express = require('express');
+const router = express.Router();
 
-app.get('/', (req, res) =>
+router.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '/public/index.html'))
-);
+});
 
-app.get('/notes', (req, res) =>
+router.get('/notes', (req, res) =>{
   res.sendFile(path.join(__dirname, '/public/notes.html'))
-);
+});
+
+router.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '/public/index.html'))
+});
 
 module.exports = router;
